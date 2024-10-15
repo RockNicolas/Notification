d5da4db589e8c205ae66d447f25bab26a75e2e8b
Nicolas Rock <146849569+RockNicolas@users.noreply.github.com>
2024-10-15 16:48:11 -0300

    Add files via upload

---
+++ notification_ponto.py
@@ -0,0 +1,30 @@
+import schedule
+import time
+from winotify import Notification, audio
+
+def enviar_notificacao():
+    try:
+        notificacao = Notification(
+            app_id="hora de bater o ponto",
+            title="HORA DO PONTO!",
+            msg="Vai bater o ponto Nicolas Rock",
+            duration="short"
+        )
+        notificacao.set_audio(audio.LoopingCall10, loop=False)
+        notificacao.add_actions(label="Portal", launch="https://intranet.icc.org.br/FrameHTML/web/app/RH/PortalMeuRH/#/login")
+        notificacao.show()
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+schedule.every().day.at("12:00").do(enviar_notificacao)
+schedule.every().day.at("16:00").do(enviar_notificacao)
+schedule.every().day.at("18:00").do(enviar_notificacao)
+
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("O script está rodando. Pressione Ctrl+C para parar.")
+    main()
